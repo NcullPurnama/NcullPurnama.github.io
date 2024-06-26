4a7d051c218d179422f1f04bc0ceeeb5931a9570
ViddyPurnama <viddyvirstandi@gmail.com>
2024-06-26 12:31:39 +0700

    add project

---
+++ script.js
@@ -0,0 +1,44 @@
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "block";
+
+    const tabs = modal.getElementsByClassName('tabcontent');
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';
+    }
+    modal.getElementsByClassName('tablinks')[0].click();
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "none";
+}
+
+function openTab(evt, tabId) {
+    const tabcontent = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    const tablinks = document.getElementsByClassName("tablinks");
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabId).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+function playSound(animalSound) {
+    const sound = new Audio(animalSound + ".mp3");
+    sound.play();
+}
+
+window.onclick = function(event) {
+    const modals = document.getElementsByClassName("modal");
+    for (let i = 0; i < modals.length; i++) {
+        if (event.target === modals[i]) {
+            modals[i].style.display = "none";
+        }
+    }
+}
